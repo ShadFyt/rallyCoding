2dd614f10494702b51ccdcf2fced5b8388cdf653
ShadFyt <doanryan10@gmail.com>
2022-02-26 22:35:13 -0600

    solved using list indexing

---
+++ python/159.py
@@ -0,0 +1,13 @@
+"""
+    You are given an array of integers and an integer N as arguments. Return the number in the array at the index of Integer N. 
+    Integer N will never be more than the length of the argument array - 1.
+"""
+
+
+def solve(int_array, n):
+    # Must return an integer
+
+    return int_array[n]
+
+
+print(solve([20, 30, 40], 0))
